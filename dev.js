8658801afb5717e5006866be27c16dc36baca66b
saltorious <cryptobet95@gmail.com>
2023-05-31 17:38:24 -0400

    updting dev

--- dev.js
+++ dev.js
@@ -12,6 +12,7 @@ var config = {
 
 var rpc = new RpcClient(config);
 var lowerCaseMethods = [];
+var lowerCaseMap = {};
 
 function setupLowerCase() {
   var methods = RpcClient.callspec;
@@ -22,6 +23,7 @@ function setupLowerCase() {
   while (n--) {
     key = keys[n];
     lowerCaseMethods[key.toLowerCase()] = methods[key];
+    lowerCaseMap[key.toLowerCase()] = key;
   }
 }
 
@@ -60,12 +62,66 @@ http
 
             if (command in lowerCaseMethods) {
               if (postData.params.length > 0) {
+                let methods = RpcClient.callspec;
+                let typeString = methods[lowerCaseMap[command] || command];
+                let types = typeString.split(" ");
+
+                let flagged = false;
+                let flagData = ''
+
+                for ([idx, type] of types.entries()) {
+                  switch (type) {
+                    case "obj":
+                      if (postData.params[idx][0] !== "{") {
+                        flagData = 'invalid obj type'
+                        flagged = true;
+                      }
+                      break;
+                    case "int":
+                      if (parseInt(postData.params[idx]).isNaN()) {
+                        flagData ='invalid int type'
+                        flagged = true;
+                      }
+                      break;
+                    case "float":
+                      if (parseFloat(postData.params[idx]).isNaN()) {
+                        flagData = 'invalid float type'
+                        flagged = true;
+                      }
+                      break;
+                    case "bool":
+                      if (typeof postData.params[idx] !== "boolean") {
+                        flagData = 'invalid bool type'
+                        flagged = true;
+                      }
+                      break;
+                    case "str":
+                      if (typeof postData.params[idx] !== "string") {
+                        flagData = 'invalid str type'
+                        flagged = true;
+                      }
+                      break;
+                  }
+                }
+
+                if (flagged) { 
+                  response.write(
+                    JSON.stringify({ error: flagData })
+                  );
+                  response.end();
+                  response.writeHead(200, "OK", {
+                    "Content-Type": "application/json",
+                  });
+                  return;
+                }
+
                 rpc[command](...postData.params, function (err, data) {
                   if (err) {
                     response.write(JSON.stringify({ error: err }));
                   } else {
                     response.write(JSON.stringify(data));
                   }
+
                   response.end();
                   response.writeHead(200, "OK", {
                     "Content-Type": "application/json",
@@ -97,8 +153,8 @@ http
         response.writeHead(200, "OK", { "Content-Type": "application/json" });
         response.end();
       }
-    } catch (e) {
-      console.log(e);
+    } catch (err) {
+      console.log(err.message);
     }
   })
   .listen(process.env.NODEPORT || 7000);
