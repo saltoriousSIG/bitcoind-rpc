f4e94b600e612f37768f3de0f3797dbb4c18d76a
Sigala Fosam <cryptobet95@gmail.com>
2023-10-06 12:47:18 -0400

    ok

--- prod.js
+++ prod.js
@@ -5,7 +5,7 @@ import dns from "dns";
 import util from "util";
 import os from "os";
 
-const RpcClient = require("./lib/index.js");
+const RpcClient = require("./lib/index.cjs");
 require("dotenv").config();
 
 const reverseDns = util.promisify(dns.reverse);
